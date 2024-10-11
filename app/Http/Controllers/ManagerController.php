b0cd918a71ea0ec0514d172c9a0cd43536631ee1
ankit kumar <ankit84334170@gmail.com>
2024-10-11 17:10:49 +0530

    add function

--- app/Http/Controllers/ManagerController.php
+++ app/Http/Controllers/ManagerController.php
@@ -9,4 +9,7 @@ class ManagerController extends Controller
     public function task(){
         echo "Task Done complete";
     }
+    public function otherTask(){
+        echo "Do Other Task";
+    }
 }
